84438037296c9290ee7252ab34e6dd584fb9f2ee
Igor Pereira Gouveia <ipgouveia@hotmail.com>
2023-11-25 22:14:50 -0300

    Adicionando versão parcial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Pixelify-sans': ['"Pixelify Sans"', 'sans-serif'],
+      'Pokemon-solid': ['"Pokemon Solid"', 'sans-serif'],
+      'Poppins': ['Poppins', 'sans-serif'],
+    },  
   },
-  plugins: [],
+  plugins: [
+  ],
 }
 
